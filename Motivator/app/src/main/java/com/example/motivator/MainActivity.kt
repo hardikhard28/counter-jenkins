eb9b9f175e7dcb24c519a3e3994ab12840236f37
hardikhard28 <63140787+hardikhard28@users.noreply.github.com>
2023-06-06 21:55:54 +0530

    added all files

--- Motivator/app/src/main/java/com/example/motivator/MainActivity.kt
+++ Motivator/app/src/main/java/com/example/motivator/MainActivity.kt
@@ -8,29 +8,40 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.example.motivator.databinding.ActivityMainBinding
+import com.google.android.material.internal.ViewUtils.hideKeyboard
 import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var name:EditText
-    private lateinit var caption:TextView
+//    private lateinit var name:EditText
+//    private lateinit var caption:TextView
+     private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val updateButton:Button=findViewById(R.id.activity_main_btn)
-        updateButton.setOnClickListener{updateMessage()}
-        name=findViewById(R.id.activity_main_et)
-        caption=findViewById((R.id.motivate_text))
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val updateButton=binding.activityMainBtn
+        updateButton.setOnClickListener{ updateMessage()}
+
+
+
+
+//        setContentView(R.layout.activity_main)
+//        val updateButton:Button=findViewById(R.id.activity_main_btn)
+//        updateButton.setOnClickListener{updateMessage()}
+//        name=findViewById(R.id.activity_main_et)
+//        caption=findViewById((R.id.motivate_text))
 
     }
     private fun updateMessage(){
-
-        val username=name.text
-        caption.text="Hey, $username "
-        name.setText("")
+    binding.motivateText.text="hehe ${binding.activityMainEt.text}";hideKeyboard()
+//        val username=name.text
+//        caption.text="Hey, $username "
+//        name.setText("")
         hideKeyboard()
     }
     private fun hideKeyboard(){
         val imm=getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(name.windowToken,0)
+        imm.hideSoftInputFromWindow(binding.activityMainEt.windowToken,0)
     }
 }
